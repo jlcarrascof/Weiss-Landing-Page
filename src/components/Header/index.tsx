ba715efe9ff016c02a3987dc705e26728f7de2b9
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-22 17:36:10 -0400

    Add inner container to Header for potential future layout elements and improve responsiveness with flexbox.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,6 +4,8 @@ const Header = () => {
   return (
     <div className="w-full md:mt-16 mt-7 md:h-[80vh] h-auto md:px-40 px-5 relative">
         <Navbar />
+        <div className="w-full md:mt-48 mt-20 flex md:flex-row flex-col items-start justify-between">
+        </div>
     </div>
   )
 }
