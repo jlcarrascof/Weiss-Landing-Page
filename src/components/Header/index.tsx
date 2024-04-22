b0d259561063b952fe41a16e5e088866a82b27fa
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-22 17:46:19 -0400

    Add introductory text element 'welcome to our website' within the Header component.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,6 +5,11 @@ const Header = () => {
     <div className="w-full md:mt-16 mt-7 md:h-[80vh] h-auto md:px-40 px-5 relative">
         <Navbar />
         <div className="w-full md:mt-48 mt-20 flex md:flex-row flex-col items-start justify-between">
+            <div className="flex flex-col md:w-auto w-full">
+                <span className="font-medium text-[#929a9f] text-[18px]">
+                    welcome to our website
+                </span>
+            </div>
         </div>
     </div>
   )
