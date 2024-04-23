f398db46e85662e804cd7ce8f8b63583686ea3e6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-22 23:22:51 -0400

    Added link-based navigation to Navbar component for medium and larger screens (md:flex)

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -14,8 +14,21 @@ const Navbar = () => {
           src={logo}
           alt="logo"
         />
+        <div className="hidden md:flex items-center gap-8">
+          {navLinks.map((link) => {
+            return (
+              <a
+                href={link.path}
+                className={``}
+                key={link.title}
+              >
+                {link.title}
+              </a>
+            );
+          })}
+        </div>
       </div>
-    )
-}
+    );
+};
 
 export default Navbar
