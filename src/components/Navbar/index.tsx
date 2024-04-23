c0a27acf78a0839f12fb5bab15f3f8627855b1fb
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-22 22:45:54 -0400

    Initialized navLinks array for Navbar component, defining basic navigation links.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,13 @@
+import logo from '../../assets/images/logo.png';
+
 const Navbar = () => {
+    const navLinks = [
+      {title: "Home", path: "/", active: true},
+      {title: "Services", path: "/", active: false},
+      {title: "Products", path: "/", active: false},
+      {title: "Newsletter", path: "/", active: false},
+      {title: "About", path: "/", active: false},
+    ]
     return (
       <div>Navbar</div>
     )
