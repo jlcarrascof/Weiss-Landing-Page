bdb98db6ccdd4a49361ea6bff8f943b75b64845a
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-05 23:00:23 -0400

    Add Services component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
+import Services from './components/Services';
 
 const App = () => {
   return (
     <div className="w-full h-full flex flex-col relative box-border">
       <Header />
+      <Services />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
