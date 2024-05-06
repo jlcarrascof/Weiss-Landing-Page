ef7a87c9d02f32a7fbf4a76c808f89b29c44d6b8
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-05 22:48:05 -0400

    Working with the Navbar component

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,8 @@
 import logo from '../../assets/images/logo.png';
 
 const Navbar = () => {
-    const navLinks = [
-      {title: "Home", path: "/", active: true},
-      {title: "Services", path: "/", active: false},
-      {title: "Products", path: "/", active: false},
-      {title: "Newsletter", path: "/", active: false},
-      {title: "About", path: "/", active: false},
-    ]
     return (
-      <div className="w-full ">
+      <div className="w-full">
         <img
           src={logo}
           alt="logo"
@@ -19,7 +12,7 @@ const Navbar = () => {
             return (
               <a
                 href={link.path}
-                className={``}
+                className={}
                 key={link.title}
               >
                 {link.title}
