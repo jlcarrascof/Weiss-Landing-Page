bc0051430d8dc926a922bdfad8d21b401d94b8dd
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-05 23:03:49 -0400

    Updated Services component to include service images.

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,3 +1,8 @@
+import service1 from '../../assets/images/service1.png'
+import service2 from '../../assets/images/service2.png'
+import service3 from '../../assets/images/service3.png'
+import about from '../../assets/images/about.png'
+
 const Services = () => {
 
     const services = [
@@ -19,7 +24,9 @@ const Services = () => {
     ];
 
     return (
-        <div>Services</div>
+        <div className="w-full md:px-40 px-5 bg-[#f5f8fa] relative z-[-1] pt-20 md:pb-[300px] pb-20">
+
+        </div>
     )
 }
 
