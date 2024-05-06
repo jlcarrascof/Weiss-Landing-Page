94d94be4b733279796978bf5c63ac955d2d369cc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-05 23:02:04 -0400

    Updated Services component to display a list of services.

--- src/components/Services/index.tsx
+++ src/components/Services/index.tsx
@@ -1,6 +1,25 @@
 const Services = () => {
+
+    const services = [
+        {
+            title: "Experienced team with great leadership skills",
+            image: service1,
+            desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime repudiandae sapiente facilis."
+        },
+        {
+            title: "Experienced team with great leadership skills",
+            image: service2,
+            desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime repudiandae sapiente facilis."
+        },
+        {
+            title: "Experienced team with great leadership skills",
+            image: service3,
+            desc:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime repudiandae sapiente facilis."
+        },
+    ];
+
     return (
-      <div>Services</div>
+        <div>Services</div>
     )
 }
 
