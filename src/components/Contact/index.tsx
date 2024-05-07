edc91b5b4abe9fbf4964910f1e8f2933b62eb252
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-07 10:54:40 -0400

    Updated the Contact component to include an appointment form.

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,13 @@
 const Contact = () => {
-    return (
-      <div>Contact</div>
-    )
+  return (
+      <div className="w-full flex md:flex-row flex-col items-start justify-between md:x-40 px-5 bg-[#f5f8fa] relative md:py-28 pt-20">
+          <div className="flex w-full justify-center flex-col gap-2">
+              <div className="font-bold text-[#263640] text-[25px] max-w-[650px] md:mt-4">
+                  Book an appointment Now!
+              </div>
+          </div>
+      </div>
+  )
 }
 
 export default Contact
