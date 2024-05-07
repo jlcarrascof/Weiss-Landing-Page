ff429e5aab1c8f55678e8e5412fc7e90595cd0ba
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-07 10:42:37 -0400

    Updated the Blog component structure to display blog data.

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,3 +1,4 @@
+
 const Blog = () => {
 
     const blogs = [
@@ -16,7 +17,13 @@ const Blog = () => {
     ];
 
     return (
-        <div>Blog</div>
+        <div className="w-full md:px-40 px-5 relative pt-20 md:pb-36 pb-20">
+            <div className="flex flex-col gap-2 md:mt-[500px]">
+                <div className="font-bold text-[#263640] text-[25px] md:max-w-[650px] max-w-full md:mt-4">
+                    What's happening in our newspaper.
+                </div>
+            </div>
+        </div>
     )
 }
 
