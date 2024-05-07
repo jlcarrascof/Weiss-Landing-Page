c509424c42e56d9bed2209646cd5b87a1ae1c060
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-07 10:39:05 -0400

    Added blog data to the Blog component.

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,6 +1,22 @@
 const Blog = () => {
+
+    const blogs = [
+        {
+            title: "The course of the disease is very important for the people",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime repudiandae sapiente facilis.",
+        },
+        {
+            title: "New special laws for constructors and their employees",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime repudiandae sapiente facilis.",
+        },
+        {
+            title: "Best materials for buildings in 2024",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime repudiandae sapiente facilis.",
+        },
+    ];
+
     return (
-      <div>Blog</div>
+        <div>Blog</div>
     )
 }
 
