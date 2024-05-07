c2e1f3294accbff1d1d0a22680901e456f17816f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-07 10:44:55 -0400

    Added text paragraphs and structure to display the latest posts in the Blog component.

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -1,4 +1,3 @@
-
 const Blog = () => {
 
     const blogs = [
@@ -21,6 +20,25 @@ const Blog = () => {
             <div className="flex flex-col gap-2 md:mt-[500px]">
                 <div className="font-bold text-[#263640] text-[25px] md:max-w-[650px] max-w-full md:mt-4">
                     What's happening in our newspaper.
+                </div>
+                <p className="text-[#929a9f] max-w-[550px] leading-7 mt-2 text-justify">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi soluta, dolor
+                    eaque consectetur ullam pariatur, itaque adipisci beatae molestiae repundiadae
+                    corrupti eos, odio neque perspiciatis ratione dignissimos voluptas.
+                </p>
+            </div>
+            <div className="flex flex-col w-full gap-3 mt-16">
+                <span className="text-[#263640] font-bold text-[20px]">
+                    Our latest posts
+                </span>
+                <div className="flex md:flex-row flex-col md:gap-0 gap-16 w-full items-center justify-between">
+                    {blogs.map((blog) => {
+                        return (
+                            <div key={blog.title} className="flex flex-col gap-3 max-w-[300px] z-50">
+
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </div>
