a3f1f7ab371271cfdc24a6856180453e64f636b6
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-07 11:05:38 -0400

    The Footer now includes a logo and navigation links.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,23 @@
+import logoWhite from "../../assets/logo-white.png"
+
 const Footer = () => {
+    const navLinks = [
+      {title: "Home", path: "/", active: true},
+      {title: "Services", path: "/", active: false},
+      {title: "Products", path: "/", active: false},
+      {title: "Newsletter", path: "/", active: false},
+      {title: "About", path: "/", active: false},
+    ]
+
     return (
-      <div>Footer</div>
+        <div className="w-full bg-[#263640] md:px-40 px-5 py-10 flex md:flex-row flex-col md:gap-0 gap-8 items-center justify-between">
+            <img src={logoWhite} alt="logo" />
+            <div className="flex items-center gap-5">
+                {navLinks.map((link) => {
+                    return <span className="font-semibold text-white">{link.title}</span>
+                })}
+            </div>
+        </div>
     )
 }
 
